1b7c5dbf523fa2a8776cb8cc193e849742e8a7b8
BeemosXD <alex@utterclarity.co.uk>
2022-05-12 19:58:44 +0100

    added new base case added new parameter and added a memo to check if a sub tree has already been calculated

--- fib.py
+++ fib.py
@@ -1,8 +1,11 @@
 # memoization
 # python object, keys will be arg to fn, value will return value
 
-def fib(n, memo = {}):
-    if n <= 2: return 1
-    return fib(n-1) + fib(n-2)
+def fib(n, memo = {}): # memo stores n as key values our return values for function
+    if (n in memo): return memo[n];
+    if (n <= 2): return 1;
+    memo[n] = fib(n-1, memo) + fib(n-2, memo); # stores result as memo
+    return memo[n];
 
 print(fib(7))
+  
