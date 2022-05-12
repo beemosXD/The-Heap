d189257a106563f8958866bc5f6e5a9cbc8a9b48
BeemosXD <alex@utterclarity.co.uk>
2022-05-12 19:53:34 +0100

    added the function fib recursive

--- fib.py
+++ fib.py
@@ -1,1 +1,8 @@
-print(1)
+# memoization
+# python object, keys will be arg to fn, value will return value
+
+def fib(n, memo = {}):
+    if n <= 2: return 1
+    return fib(n-1) + fib(n-2)
+
+print(fib(7))
